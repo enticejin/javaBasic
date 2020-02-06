c5200c0cf7807cdfd1fa121d693d26d110890875
少爷辉 <www.403367632@qq.com>
2020-02-06 20:59:18 +0800

    java基础线程睡眠时间

---
+++ javaThread/MyThread.java
@@ -0,0 +1,23 @@
+package javaThread;
+
+public class MyThread extends Thread {
+
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            if ((i) % 10 == 0) {
+                System.out.println("-------" + i);
+            }
+            System.out.print(i);
+            try {
+                Thread.sleep(1);
+                System.out.print("    线程睡眠1毫秒！\n");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new MyThread().start();
+    }
+}
