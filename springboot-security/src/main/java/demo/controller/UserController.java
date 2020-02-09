790758a0f3d8f0bd2d22012906f9558f115d84d5
少爷辉 <www.403367632@qq.com>
2020-02-09 23:58:21 +0800

    添加api文档说明

--- springboot-security/src/main/java/demo/controller/UserController.java
+++ springboot-security/src/main/java/demo/controller/UserController.java
@@ -1,5 +1,7 @@
 package demo.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,30 +25,38 @@ import org.springframework.web.bind.annotation.ResponseBody;
  *
  * 以用户名 admin 登录，不可以进入系统，因为系统中还没有该用户。
  */
+
+//@Api：用在类上，说明该类的作用。
+//@ApiOperation：说明该方法的作用。
 @Controller
 @RequestMapping("/user")
+@Api(value = "用户模块说明")
 public class UserController {
 
     @RequestMapping("/addUser")
     @ResponseBody
+    @ApiOperation(value = "添加用户",notes = "放一些信息，供测试判断")
     public String addUser(){
         return "这是进行用户的添加";
     }
 
     @RequestMapping("/deleteUser")
     @ResponseBody
+    @ApiOperation(value = "删除用户",notes = "放一些信息，供测试判断")
     public String deleteUser(){
         return "这是进行用户的删除操作";
     }
 
     @RequestMapping("/updateUser")
     @ResponseBody
+    @ApiOperation(value = "更改用户操作",notes = "放一些信息，供测试判断")
     public String updateUser(){
         return "这是进行用户的修改操作";
     }
 
     @RequestMapping("/findAllUsers")
     @ResponseBody
+    @ApiOperation(value = "查询所有用户",notes = "放一些信息，供测试判断")
     public String findAllUsers(){
         return "这是进行查询所有的用户";
     }
