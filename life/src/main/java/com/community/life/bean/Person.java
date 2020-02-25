aa3f3157914ea0d0b93c3cd3b9923a99ae72e252
少爷辉 <www.403367632@qq.com>
2020-02-25 20:59:06 +0800

    springboot配置文件中的值获取方式

---
+++ life/src/main/java/com/community/life/bean/Person.java
@@ -0,0 +1,96 @@
+package com.community.life.bean;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Component
+//获取配置文件中的对象对应值ConfigurationProperties
+//@ConfigurationProperties(prefix = "person")
+public class Person {
+    //@Value也可以获取值
+    @Value("${person.last-name}")
+    private String lastName;
+    @Value("#{11 * 2 }")
+    private Integer age;
+    @Value("true")
+    private Boolean boss;
+    private Date birth;
+
+    private List<Object> lists;
+    private Map<String, Object> maps;
+    private Dog dog;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Boolean getBoss() {
+        return boss;
+    }
+
+    public void setBoss(Boolean boss) {
+        this.boss = boss;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public List<Object> getLists() {
+        return lists;
+    }
+
+    public void setLists(List<Object> lists) {
+        this.lists = lists;
+    }
+
+    public Map<String, Object> getMaps() {
+        return maps;
+    }
+
+    public void setMaps(Map<String, Object> maps) {
+        this.maps = maps;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", boss=" + boss +
+                ", birth=" + birth +
+                ", lists=" + lists +
+                ", maps=" + maps +
+                ", dog=" + dog +
+                '}';
+    }
+}
