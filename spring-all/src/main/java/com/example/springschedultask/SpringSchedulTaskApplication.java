050d11b3b97160c1d7093b11dafdd86201fd0a7f
Administrator <www.403367632@qq.com>
2020-02-28 10:42:46 +0800

    springboot-任务规划

---
+++ spring-all/src/main/java/com/example/springschedultask/SpringSchedulTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.springschedultask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchedulTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSchedulTaskApplication.class, args);
+    }
+
+}
---
+++ spring-all/src/test/java/com/example/springschedultask/SpringSchedulTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springschedultask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSchedulTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
