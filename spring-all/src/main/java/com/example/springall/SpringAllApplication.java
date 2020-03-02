9e7202c4af7248591e94f6fe5fbad1b74d1a81c6
Administrator <www.403367632@qq.com>
2020-03-02 09:48:21 +0800

    springboot-文件上传

--- spring-all/src/main/java/com/example/springall/SpringAllApplication.java
+++ spring-all/src/main/java/com/example/springall/SpringAllApplication.java
@@ -1,13 +1,13 @@
-package com.example.springschedultask;
+package com.example.springall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSchedulTaskApplication {
+public class SpringAllApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringSchedulTaskApplication.class, args);
+        SpringApplication.run(SpringAllApplication.class, args);
     }
 
 }
--- spring-all/src/test/java/com/example/springall/SpringAllApplicationTests.java
+++ spring-all/src/test/java/com/example/springall/SpringAllApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.springschedultask;
+package com.example.springall;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringSchedulTaskApplicationTests {
+class SpringAllApplicationTests {
 
     @Test
     void contextLoads() {
