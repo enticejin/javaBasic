b7ff21b79e2ab7bd23c7eb24ffa060d5e04bd574
Administrator <www.403367632@qq.com>
2020-05-21 11:17:41 +0800

    springboot整合handling-form-submission

---
+++ springboot-handling-form-submission/src/main/java/com/example/handlingformsubmission/Greeting.java
@@ -0,0 +1,30 @@
+package com.example.handlingformsubmission;
+
+public class Greeting {
+    private  long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
