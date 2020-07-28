c867379b8c3dab6f0abf537ccd604c0480c89781
Administrator <www.403367632@qq.com>
2020-07-28 16:40:10 +0800

    springboot-activeMQ

---
+++ springboot-activeMq/src/main/java/com/example/activemq/ActiveMqApplication.java
@@ -0,0 +1,14 @@
+package com.example.activemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
+
+@SpringBootApplication
+public class ActiveMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActiveMqApplication.class, args);
+    }
+
+}
